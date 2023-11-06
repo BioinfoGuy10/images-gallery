8a9dfdc4e4f04b8e6753115d9a3d3aa573f39471
Keith Saldanha <saldanhakeith1@gmail.com>
2023-11-06 15:45:22 +0530

    Initial frontend app

---
+++ frontend/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './css/index.css';
+import App from './App';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+    <h1>Hello from react</h1>
+  </React.StrictMode>
+);
+
