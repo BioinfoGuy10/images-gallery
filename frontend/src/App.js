074576bda693fe1bc17c8c87f8ebd294483fca77
Keith Saldanha <saldanhakeith1@gmail.com>
2023-11-14 17:11:06 +0530

    Save images in the state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,15 +6,18 @@ import Header from './components/header';
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 const App = () => {
   const [word, setWord] = useState('');
+  const [images, setImages] = useState([]);
+  console.log(images);
+
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     console.log(word);
     fetch(
-      'https://api.unsplash.com/photos/random/?query={word}&client_id=${UNSPLASH_KEY}',
+      `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`,
     )
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        setImages([data, ...images]);
       })
       .catch((err) => {
         console.log(err);
